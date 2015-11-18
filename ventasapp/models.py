80f49f1854cf1f26af0e21e4a9aa005352528a37
Daniel-Orozco <geoluisd@hotmail.com>
2015-11-17 20:54:49 -0800

    First model definitions

--- ventasapp/models.py
+++ ventasapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Sale(models.Model):
+    date_created = models.DateTimeField('date created')
+    subtotal = models.DecimalField(decimal_places=2)
+    tax = models.IntegerField()
+    payment = models.DecimalField(decimal_places=2)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    unit_cost = models.DecimalField(decimal_places=2)
+
+
+class Sales-Products(models.Model):
+    sale_id = models.ForeignKey('Sale')
+    product_id = models.ForeignKey('Product')
+    quanitity = models.DecimalField(decimal_places=3)
